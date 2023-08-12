ce8d20256108c05773f3e3141c54ee06a4c729e9
danilovict2 <danilosindric@gmail.com>
2023-08-12 19:00:55 +0200

    Added forgot password

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Vue\VueBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
