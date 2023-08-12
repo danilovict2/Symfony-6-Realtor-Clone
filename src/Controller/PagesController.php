11c6c70105fcd5feb69c3417341190e013b26c0c
danilovict2 <danilosindric@gmail.com>
2023-08-12 18:00:02 +0200

    added navbar

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class PagesController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('pages/index.html.twig', [
+            
         ]);
     }
 }
